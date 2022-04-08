26fbae7af2bf88c4b058a0b21d4a7a5dc2a3c8e5
Juliana Lima <julianalimab88@gmail.com>
2022-04-08 16:25:15 -0300

    Carrossel okay

---
+++ script.js
@@ -0,0 +1,60 @@
+const btnMobile = document.getElementById("btn-mobile");
+const mySlide = document.querySelectorAll(".myslider");
+const dot = document.querySelectorAll(".dot");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expended", "true");
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
+
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoslide, 8000);
+function autoslide() {
+  counter += 1;
+  slidefun(counter);
+}
+function plusSlides(n) {
+  counter += n; 
+  slidefun(counter);
+  resetTimer();
+}
+function currentSlide(n) {
+  counter = n;
+  slidefun(counter);
+  resetTimer();
+}
+function resetTimer() {
+  clearInterval(timer);
+  timer = setInterval(autoslide, 8000);
+}
+
+function slidefun(n) {
+  let i;
+  for (i = 0; i < mySlide.length; i++) {
+    mySlide[i].style.display = "none";
+  }
+  for (i = 0; i < dot.length; i++) {
+    dot[i].classList.remove("active");
+  }
+  if (n > mySlide.length) {
+    counter = 1;
+  }
+  if (n < 1) {
+    counter = mySlide.length;
+  }
+  mySlide[counter - 1].style.display = "block";
+  dot[counter - 1].classList.add("active");
+}
